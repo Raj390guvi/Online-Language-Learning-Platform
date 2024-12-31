eb49f0a269a443a857696f297f8f2e5b53337b4c
Raj390guvi <r39068182@gmail.com>
2024-12-31 10:16:58 +0530

    Create CourseService.java

---
+++ CourseService.java
@@ -0,0 +1,22 @@
+package com.example.languagelearning.service;
+
+import com.example.languagelearning.model.Course;
+import com.example.languagelearning.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CourseService {
+    @Autowired
+    private CourseRepository courseRepository;
+
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    public Course createCourse(Course course) {
+        return courseRepository.save(course);
+    }
+}
