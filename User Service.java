733b23854e2d31983c590130cfe1659981a9029b
Raj390guvi <r39068182@gmail.com>
2024-12-31 10:22:00 +0530

    Create User Service.java

---
+++ User Service.java
@@ -0,0 +1,20 @@
+package com.example.languagelearning.service;
+
+import com.example.languagelearning.model.User;
+import com.example.languagelearning.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User registerUser (User user) {
+        return userRepository.save(user);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
