ee9ba5bb1bfbee9b487a7d5de93d4aca6aa61de4
Raj390guvi <r39068182@gmail.com>
2024-12-31 10:23:39 +0530

    Lessonrepository.java

---
+++ Lessonrepository.java
@@ -0,0 +1,6 @@
+package com.example.languagelearning.repository;
+
+import com.example.languagelearning.model.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LessonRepository extends JpaRepository<
