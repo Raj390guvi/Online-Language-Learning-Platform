e6c903a5d757b50fa897e4c93ee0b1931cff60ba
Raj390guvi <r39068182@gmail.com>
2025-01-07 21:04:15 +0530

    Create RegisterServlet.java

---
+++ Review 3 & 4/RegisterServlet.java
@@ -0,0 +1,16 @@
+package com.example.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Registration logic here
+    }
+}
